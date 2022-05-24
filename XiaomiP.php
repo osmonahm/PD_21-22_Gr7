9c2b75d3fc463ff6fa56d8f732b5d30625e3e783
Osmon Ahmagjekaj <osmon.ahh@gmail.com>
2022-05-24 22:55:39 +0200

    Slight changes

--- XiaomiP.php
+++ XiaomiP.php
@@ -6,7 +6,7 @@ require('header.php');
 
 <div style ="height: 200px;"; id="bannerImage">
 <h1>PhoneShop - telefonat e fundit</h1>
-    <p>Koleksioni jone i Xiaomi.</p>
+    <p>Koleksioni jonë i Xiaomi.</p>
 
 </div>
 
